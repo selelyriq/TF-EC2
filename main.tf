a88fb52782745d7a92228ddee1f5f7acd5831f51
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-03-30 21:24:42 -0400

    fmt

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 4.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
 }
 
 resource "aws_instance" "ec2_instance" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key_name
-    user_data = var.user_data
-    tags = merge(var.tags, { Name = var.name })   
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  user_data     = var.user_data
+  tags          = merge(var.tags, { Name = var.name })
 }
 
 resource "aws_eip" "ec2_eip" {
-    instance = aws_instance.ec2_instance.id
-    tags = merge(var.tags, { Name = var.name })   
+  instance = aws_instance.ec2_instance.id
+  tags     = merge(var.tags, { Name = var.name })
 }
 
 resource "aws_eip_association" "eip_assoc" {
-    instance_id = aws_instance.ec2_instance.id
-    allocation_id = aws_eip.ec2_eip.id
+  instance_id   = aws_instance.ec2_instance.id
+  allocation_id = aws_eip.ec2_eip.id
 }
 
--- variables.tf
+++ variables.tf
@@ -1,32 +1,32 @@
 variable "name" {
-    type = string
-    description = "The name of the EC2 instance"
+  type        = string
+  description = "The name of the EC2 instance"
 }
 
 variable "ami_id" {
-    type = string
-    description = "The AMI ID to use for the EC2 instance"
+  type        = string
+  description = "The AMI ID to use for the EC2 instance"
 }
 
 
 variable "instance_type" {
-    type = string
-    description = "The instance type to use for the EC2 instance"
+  type        = string
+  description = "The instance type to use for the EC2 instance"
 }
 
 
 variable "key_name" {
-    type = string
-    description = "The key name to use for the EC2 instance"
+  type        = string
+  description = "The key name to use for the EC2 instance"
 }
 
 variable "user_data" {
-    type = string
-    description = "The user data to use for the EC2 instance"
+  type        = string
+  description = "The user data to use for the EC2 instance"
 }
 
 
 variable "tags" {
-    type = map(string)
-    description = "The tags to use for the EC2 instance"
+  type        = map(string)
+  description = "The tags to use for the EC2 instance"
 }
